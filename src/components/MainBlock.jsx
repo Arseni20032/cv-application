68f30e0fe15078d530bc36f54facbe652682b94f
Арсений Тепляков <arsenijteplakov@MacBook-Air-Arsenij.local>
2024-10-16 19:22:55 +0300

    breaked custom hooks into different files

--- src/components/MainBlock.jsx
+++ src/components/MainBlock.jsx
@@ -2,79 +2,26 @@ import { useState } from "react";
 import GeneralInfoBlock from "./blockForms/GeneralInfoBlock";
 import EducationBlock from "./blockForms/EducationBlock";
 import ExperienceBlock from "./blockForms/ExperienceBlock";
+import useFormManagement from "./hooks/useFormManagement";
 import CVBlock from "./displayInfo/CV";
 import "../styles/MainBlockStyle.scss";
 
 export default function MainBlockForm() {
-    const [showMoreEducation, setShowMoreEducation] = useState(false);
-    const [showMoreExperience, setShowMoreExperience] = useState(false);
-    const [experiences, setExperiences] = useState([]); 
-    const [educations, setEducations] = useState([]); 
-    const [formData, setFormData] = useState({
-        name: '',
-        phone: '',
-        email: '',
-        address: '',
-        startDateEducation: '',
-        endDateEducation: '',
-        locationEducation: '',
-        education: '',
-        qualification: '',
-        experience: '',
-        startDateExperience: '',
-        endDateExperience: '',
-        locationExperience: '',
-        description: ''
-    });
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value
-        }));
-    };
-
-    const handleShowEducation = () => {
-        setShowMoreEducation((prevState) => !prevState);
-        setShowMoreExperience(false);
-    };
-
-    const handleShowExperience = () => {
-        setShowMoreExperience((prevState) => !prevState);
-        setShowMoreEducation(false);
-    };
-
-    const handleSaveExperience = () => {
-        setExperiences((prevExperiences) => [...prevExperiences, formData]);
-        setFormData({
-            ...formData,
-            experience: '',
-            startDateExperience: '',
-            endDateExperience: '',
-            locationExperience: '',
-            description: ''
-        });
-    };
-
-    const handleDeleteExperience = () => {
-        setExperiences((prevExperiences) => prevExperiences.slice(0, -1));
-    };
-
-    const handleSaveEducation = () => {
-        setEducations((prevEducations) => [...prevEducations, formData]);
-        setFormData({
-            ...formData,
-            education: '',
-            qualification: '',
-            startDateEducation: '',
-            endDateEducation: '',
-            locationEducation: '',
-        })
-    }
-
-    const handleDeleteEducation = () => setEducations((prevEducations) => prevEducations.slice(0, -1));
-
+    const {
+        formData,
+        showMoreEducation,
+        showMoreExperience,
+        experiences,
+        educations,
+        handleInputChange,
+        handleShowEducation,
+        handleShowExperience,
+        handleSaveEducation,
+        handleDeleteEducation,
+        handleSaveExperience,
+        handleDeleteExperience,
+    } = useFormManagement();
+    
     return (
         <div className="main-block">
             <div className="content-block">
---
+++ src/components/hooks/useFormManagement.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+
+export default function useFormManagement() {
+    const [showMoreEducation, setShowMoreEducation] = useState(false);
+    const [showMoreExperience, setShowMoreExperience] = useState(false);
+    const [experiences, setExperiences] = useState([]);
+    const [educations, setEducations] = useState([]);
+    const [formData, setFormData] = useState({
+        name: '',
+        phone: '',
+        email: '',
+        address: '',
+        education: '',
+        qualification: '',
+        startDateEducation: '',
+        endDateEducation: '',
+        locationEducation: '',
+        experience: '',
+        startDateExperience: '',
+        endDateExperience: '',
+        locationExperience: '',
+        description: ''
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    const handleShowEducation = () => {
+        setShowMoreEducation(!showMoreEducation);
+        setShowMoreExperience(false);
+    };
+
+    const handleShowExperience = () => {
+        setShowMoreExperience(!showMoreExperience);
+        setShowMoreEducation(false);
+    };
+
+    const handleSaveEducation = () => {
+        setEducations([...educations, formData]);
+        setFormData((prev) => ({
+            ...prev,
+            education: '',
+            qualification: '',
+            startDateEducation: '',
+            endDateEducation: '',
+            locationEducation: '',
+        }));
+    };
+
+    const handleDeleteEducation = () => setEducations(educations.slice(0, -1));
+
+    const handleSaveExperience = () => {
+        setExperiences([...experiences, formData]);
+        setFormData((prev) => ({
+            ...prev,
+            experience: '',
+            startDateExperience: '',
+            endDateExperience: '',
+            locationExperience: '',
+            description: ''
+        }));
+    };
+
+    const handleDeleteExperience = () => setExperiences(experiences.slice(0, -1));
+
+    return {
+        formData,
+        showMoreEducation,
+        showMoreExperience,
+        experiences,
+        educations,
+        handleInputChange,
+        handleShowEducation,
+        handleShowExperience,
+        handleSaveEducation,
+        handleDeleteEducation,
+        handleSaveExperience,
+        handleDeleteExperience,
+    };
+}
